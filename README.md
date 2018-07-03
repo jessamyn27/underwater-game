5c6f50b1e8c1d16b1bae83357760478276f5cf6c
Jessamyn McTwigan <jessamyn.mctwigan@gmail.com>
2018-07-03 13:56:25 -0500

    getting all images and background on

---
+++ README.md
@@ -0,0 +1,11 @@
+Underwater Game!
+
+credits:
+
+background:
+<a href="http://www.freepik.com">Designed by Sketchepedia / Freepik</a>
+
+icons:
+// All icons are licensed under Creative Commons (http://creativecommons.org/licenses/by/3.0/) and taken from FlatIcon (https://www.flaticon.com). Additional attributes are as following:
+
+Mermaid, Shell, Hook = Icons made by FreePik (http://www.freepik.com).
--- app.js
+++ app.js
@@ -1,12 +1,84 @@
-console.log("canvas tutorial");
+console.log("underwater game with canvas");
 
+// Get canvas element and define context
 const canvas = document.getElementById('canvas');
-var c = canvas.getContext('2d');
+const c = canvas.getContext('2d');
 
+// Sets canvas width
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-console.log(canvas);
+// Makes canvas width responsive
+window.addEventListener('resize', function() {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+});
+
+//CLASSES
+class Mermaid {
+  constructor(name, speed, speedType, direction, xPosition, yPosition, width, height) {
+    this.name = name;
+    this.speed = speed;
+    this.speedType = speedType;
+    this.direction = direction;
+    this.xPosition = xPosition;
+    this.yPosition = yPosition;
+    this.width = width;
+    this.height = height;
+  }
+}
+
+const mermaid = new Mermaid('mermaid', 4, 'medium', 'all', 660, 510, 128, 128);
+
+class Shell extends Mermaid {}
+
+const pink = new Shell('pink', 5, 'fast', 'left')
+for (var i = 0; i < 400; i++) {
+  var x = Math.random() * window.innerWidth;
+  var y = Math.random() * window.innerHeight;
+  $pink.append('shellImage');
+}
+//  images
+let backgroundImage = new Image();
+backgroundImage.src = 'images/background.jpg'
+
+let shellImage = new Image();
+shellImage.src = 'images/shell.png'
+
+let mermaidImage = new Image();
+mermaidImage.src = 'images/mermaid.png'
+
+let fishingImage = new Image();
+fishingImage.src = 'images/fishing.png'
+
+// function to show & size my images
+const animate = () => {
+c.clearRect(0, 0, 1000, 1000);
+requestAnimationFrame(animate);
+c.drawImage(backgroundImage, 0, 0, 1000, 1000);
+c.drawImage(shellImage, 0, 110, 100, 100);
+c.drawImage(mermaidImage, 200, 200, 100, 100);
+c.drawImage(fishingImage, 400, 10, 100, 100)
+}
+animate();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // c.fillStyle = "rgba(255, 0, 0, 0.5)";
 // c.fillRect(100, 100, 100, 100);
@@ -26,6 +98,8 @@ console.log(canvas);
 // c.strokeStyle = "lavender";
 // c.stroke();
 
+/////////////////////////////////// circles ////////
+
 // arc or circle x, y, radius, startAngle endAngle draw counter clockwise
 // c.beginPath();
 // c.arc(300, 300, 30, 0, Math.PI * 2, false);
@@ -40,107 +114,107 @@ console.log(canvas);
 // c.strokeStyle = 'blue';
 // c.stroke();
 // }
-var mouse = {
-  x: undefined,
-  y: undefined
-}
-var maxRadius = 40;
-// var minRadius = 2;
-
-var colorArray = [
-  'pink',
-  'lavender',
-  'beige',
-  'blue',
-  'green',
-]
-
-window.addEventListener('mousemove',
-function (event) {
-mouse.x = event.x;
-mouse.y = event.y;
-})
-
-window.addEventListener('resize', function()
-{
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
-
-  // itit();
-
-});
-
-function Circle(x, y, dx, dy, radius) {
-  this.x = x;
-  this.y = y;
-  this.dx = dx;
-  this.dy = dy;
-  this.radius = radius;
-  this.minRadius = radius;
-  this.color = colorArray[Math.floor(Math.random() * colorArray.length)];
-
-  this.draw = function() {
-    c.beginPath();
-    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false
-      );
-    c.strokeStyle = this.color;
-    c.stroke();
-    // c.fill():
-
-  }
-
-  this.update = function() {
-    if (this.x + this.radius > innerWidth ||
-      this.x - this.radius < 0) {
-      this.dx = -this.dx;
-    }
-    if (this.y + this.radius > innerHeight ||
-      this.y - this.radius < 0) {
-      this.dy = -this.dy;
-    }
-    this.x += this.dx;
-    this.y += this.dy;
-
-// this is interactivity with the mouse
-  if (mouse.x - this.x < 50 && mouse.x - this.x > -50
-    && mouse.y - this.y < 50 && mouse.y - this.y > -50)
-     {
-       if (this.radius < maxRadius) {
-         this.radius += 1;
-
-       }
-  } else if (this.radius > this.minRadius) {
-      this.radius -= 1;
-
-  }
-
-    this.draw();
-  }
-}
-
-
-
-var circleArray = [];
-
-// function init() {
-  for (var i = 0; i < 8000; i++) {
-  var radius = Math.random() * 3 + 1;
-  var x = Math.random() * (innerWidth - radius * 2) + radius;
-  var y = Math.random() * (innerHeight - radius * 2) + radius;
-  var dx = (Math.random() -0.5) * 2;
-  var dy = (Math.random() -0.5) * 2;
-  circleArray.push(new Circle(x, y, dx, dy, radius));
-  }
+// var mouse = {
+//   x: undefined,
+//   y: undefined
 // }
-
-function animate() {
-  requestAnimationFrame(animate);
-  c.clearRect(0, 0, innerWidth, innerHeight
-    );
-    for (var i = 0; i < circleArray.length; i++) {
-      circleArray[i].update();
-    }
-}
+// var maxRadius = 40;
+// // var minRadius = 2;
+//
+// var colorArray = [
+//   'pink',
+//   'lavender',
+//   'beige',
+//   'blue',
+//   'green',
+// ]
+//
+// window.addEventListener('mousemove',
+// function (event) {
+// mouse.x = event.x;
+// mouse.y = event.y;
+// })
+//
+// window.addEventListener('resize', function()
+// {
+//   canvas.width = window.innerWidth;
+//   canvas.height = window.innerHeight;
+//
+//   // itit();
+//
+// });
+//
+// function Circle(x, y, dx, dy, radius) {
+//   this.x = x;
+//   this.y = y;
+//   this.dx = dx;
+//   this.dy = dy;
+//   this.radius = radius;
+//   this.minRadius = radius;
+//   this.color = colorArray[Math.floor(Math.random() * colorArray.length)];
+//
+//   this.draw = function() {
+//     c.beginPath();
+//     c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false
+//       );
+//     c.strokeStyle = this.color;
+//     c.stroke();
+//     // c.fill():
+//
+//   }
+//
+//   this.update = function() {
+//     if (this.x + this.radius > innerWidth ||
+//       this.x - this.radius < 0) {
+//       this.dx = -this.dx;
+//     }
+//     if (this.y + this.radius > innerHeight ||
+//       this.y - this.radius < 0) {
+//       this.dy = -this.dy;
+//     }
+//     this.x += this.dx;
+//     this.y += this.dy;
+//
+// // this is interactivity with the mouse
+//   if (mouse.x - this.x < 50 && mouse.x - this.x > -50
+//     && mouse.y - this.y < 50 && mouse.y - this.y > -50)
+//      {
+//        if (this.radius < maxRadius) {
+//          this.radius += 1;
+//
+//        }
+//   } else if (this.radius > this.minRadius) {
+//       this.radius -= 1;
+//
+//   }
+//
+//     this.draw();
+//   }
+// }
+//
+//
+//
+// var circleArray = [];
+//
+// // function init() {
+//   for (var i = 0; i < 8000; i++) {
+//   var radius = Math.random() * 3 + 1;
+//   var x = Math.random() * (innerWidth - radius * 2) + radius;
+//   var y = Math.random() * (innerHeight - radius * 2) + radius;
+//   var dx = (Math.random() -0.5) * 2;
+//   var dy = (Math.random() -0.5) * 2;
+//   circleArray.push(new Circle(x, y, dx, dy, radius));
+//   }
+// // }
+//
+// function animate() {
+//   requestAnimationFrame(animate);
+//   c.clearRect(0, 0, innerWidth, innerHeight
+//     );
+//     for (var i = 0; i < circleArray.length; i++) {
+//       circleArray[i].update();
+//     }
+// }
   // c.beginPath();
   // c.arc(x, y, radius, 0, Math.PI * 2, false
   //   );
@@ -149,4 +223,4 @@ function animate() {
 
 // itit();
 
-animate();
+// animate();
