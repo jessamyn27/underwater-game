35f6e738d8cf61d0e30fa872c817dd749231982d
Jessamyn McTwigan <jessamyn.mctwigan@gmail.com>
2018-07-05 16:31:56 -0500

    shell gone crazy

--- app.js
+++ app.js
@@ -25,35 +25,38 @@ class Floater {
     this.dx = dx;
     this.dy = dy;
     this.image = image;
-    this.hasBeenMoving = 0;
+    this.hasBeenMoving = 8;
 }
-move(direction){
-    console.log("MOVING")
-      if(direction == "up"){
-        // move up
-        this.y -= this.dy;
-      }else if(direction =="down"){
-      // move down
-        this.y += this.dy;
-      }else if(direction == "right"){
-        // move right
-          this.x += this.dx
-      }else if(direction =="left"){
-        // move left
-          this.x -= this.dx
-      }
-    this.hasBeenMoving++;
-    if(this.hasBeenMoving > 3){
-        clearInterval(this.floatingAround)
+// move(direction){
+//     console.log("MOVING")
+//       if(direction == "up" && this.y - this.dy < 0){
+//         // move up
+//         this.y -= this.dy;
+//       }else if(direction =="down" && this.y + this.dy > canvas.height ){
+//       // move down
+//         this.y += this.dy;
+//       }else if(direction == "right" && this.x + this.dx > canvas.width){
+//         // move right
+//         this.x += this.dx
+//       }else if(direction =="left" && this.x - this.dx < 0){
+//         // move left
+//           this.x -= this.dx
+//       }
+//     this.hasBeenMoving++;
+//     if(this.hasBeenMoving > 3){
+//         clearInterval(this.floatingAround)
+//         this.float()
     }
+
 }
+
 float(){
-  console.log("floating around");
-  let randomDirections = ["up", "down", "left", "right"]
-  let randomDirection = randomDirections[Math.floor(Math.random() * randomDirections.length)];
-    this.hasBeenMoving = 0;
-    this.floatingAround = setInterval(()=>{
-        this.move(Math.random())}, 500)
+ console.log("floating around");
+ let randomDirections = ["up", "down", "left", "right"]
+ let randomDirection = randomDirections[Math.floor(Math.random() * randomDirections.length)];
+   this.hasBeenMoving = 0;
+   this.floatingAround = setInterval(()=>{
+       this.move(randomDirection)}, 100)
 }
 }
 // float
