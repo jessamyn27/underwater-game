6776b8e4566eb3a37117e59294c118804d44b950
Jessamyn McTwigan <jessamyn.mctwigan@gmail.com>
2018-07-06 10:47:25 -0500

    resizing

--- app.js
+++ app.js
@@ -17,10 +17,11 @@ window.addEventListener('resize', function() {
 window.addEventListener("keydown", moveAriel, true);
 
 var gameOver = false;
+var points = 0;
 
 //CLASSES
 class Floater {
-  constructor(name, x, y, dx, dy, image) {
+  constructor(name, x, y, dx, dy, image,) {
     this.name = name;
     this.x = x;
     this.y = y;
@@ -31,74 +32,75 @@ class Floater {
     this.hasBeenMoving = 0;
     // this.height = height;
     // this.width = wid
-}
-move(direction){
+  }
+  move(direction) {
     // console.log("MOVING")
-      if(direction == "up" && this.y - this.dy > 0){
-        // move up
-        this.y -= this.dy;
-      }else if(direction =="down" && this.y + this.dy < canvas.height ){
+    if (direction == "up" && this.y - this.dy > 0) {
+      // move up
+      this.y -= this.dy;
+    } else if (direction == "down" && this.y + this.dy < canvas.height) {
       // move down
-        this.y += this.dy;
-      }else if(direction == "right" && this.x + this.dx < canvas.width){
-        // move right
-        this.x += this.dx
-      }else if(direction =="left" && this.x - this.dx > 0){
-        // move left
-          this.x -= this.dx
-      }
-    this.hasBeenMoving++;
-    if(this.hasBeenMoving > 10){
-        clearInterval(this.floatingAround)
-        this.float()
+      this.y += this.dy;
+    } else if (direction == "right" && this.x + this.dx < canvas.width) {
+      // move right
+      this.x += this.dx
+    } else if (direction == "left" && this.x - this.dx > 0) {
+      // move left
+      this.x -= this.dx
     }
-}
+    this.hasBeenMoving++;
+    if (this.hasBeenMoving > 10) {
+      clearInterval(this.floatingAround)
+      this.float()
+    }
+  }
 
-float(){
- // console.log("floating around");
- let randomDirections = ["up", "down", "left", "right"]
- let randomDirection = randomDirections[Math.floor(Math.random() * randomDirections.length)];
-   this.hasBeenMoving = 0;
-   this.floatingAround = setInterval(()=>{
-       this.move(randomDirection)}, 70)
-}
+  float() {
+    // console.log("floating around");
+    let randomDirections = ["up", "down", "left", "right"]
+    let randomDirection = randomDirections[Math.floor(Math.random() * randomDirections.length)];
+    this.hasBeenMoving = 0;
+    this.floatingAround = setInterval(() => {
+      this.move(randomDirection)
+    }, 70)
+  }
 }
 // float
 // need to make the mermaid object
-const ariel = new Floater('ariel', 500, 500, 50, 50);
+const ariel = new Floater('ariel', 50, 50, 50, 50);
 ariel.height = 100
 ariel.width = 100
 
 // need to make the shell object
-const shell = new Floater('shell', 300, 300, 100, 100);
+const shell = new Floater('shell', 400, 400, 100, 100);
 shell.float()
 shell.height = 100
-shell.width = 200
+shell.width = 100
 
 
 // need to make the shell object
-const clam = new Floater('clam', 300, 300, 200, 200);
+const clam = new Floater('clam', 500, 500, 200, 200);
 clam.float()
 clam.height = 100
-clam.width = 300
+clam.width = 100
 
 // need to make the anchor object
-const anchor = new Floater('anchor', 200, 200, 100, 100);
+const anchor = new Floater('anchor', 1000, 1000, 100, 100);
 anchor.float()
-anchor.height = 100
-anchor.width = 400
+anchor.height = 500
+anchor.width = 500
 
 // need to make the anchor object
-const snail = new Floater('snail', 400, 400, 10, 10);
+const snail = new Floater('snail', 800, 800, 10, 10);
 snail.float()
 snail.height = 100
-snail.width = 500
+snail.width = 100
 
 // need to make the anchor object
 const danger = new Floater('danger', 400, 400, 10, 10);
 // danger.float()
 danger.height = 100
-danger.width = 500
+danger.width = 100
 
 //  images showing up
 const backgroundImage = new Image();
@@ -122,101 +124,110 @@ snailImage.src = 'images/sea-snail.png'
 const dangerImage = new Image();
 dangerImage.src = 'images/danger.png'
 
-const hide = () => {
-  shell.x = 200;
-  shell.y = 200;
-}
+// const hide = () => {
+//   shell.x = 200;
+//   shell.y = 200;
+// }
 // function to show & size my images on the screen
 const animate = () => {
-c.clearRect(0, 0, canvas.width, canvas.height);
-requestAnimationFrame(animate);
-if (ariel.x < shell.x + shell.width &&
-   ariel.x + ariel.width > shell.x &&
-   ariel.y < shell.y + shell.height &&
-   ariel.height + ariel.y > shell.y) {
-shell.isVisible = false;
-// gameOver = true;
-// ariel.style.display="none";
-// c.shell.remove();
-// shell.splice(i);
-}
-if (ariel.x < anchor.x + anchor.width &&
-   ariel.x + ariel.width > anchor.x &&
-   ariel.y < anchor.y + anchor.height &&
-   ariel.height + ariel.y > anchor.y) {
-     alert("game over");
-     gameOver = true;
-// console.log("hook!");
-// alert("argh! merbabe got hooked!")
-}
-if (ariel.x < clam.x + clam.width &&
-   ariel.x + ariel.width > clam.x &&
-   ariel.y < clam.y + clam.height &&
-   ariel.height + ariel.y > clam.y) {
-clam.isVisible = false;
-}
-if (ariel.x < snail.x + snail.width &&
-   ariel.x + ariel.width > snail.x &&
-   ariel.y < snail.y + snail.height &&
-   ariel.height + ariel.y > snail.y) {
-     clam.isVisible = false;
-     // console.log('snail overlapped');
-// snail.visible = false;
-// snail = null;
-// console.log("snail!");
-}
-c.drawImage(backgroundImage, 0, 0, canvas.width, canvas.height);
-if (shell.isVisible)
-  c.drawImage(shellImage, shell.x, shell.y, 100, 100);
+  c.clearRect(0, 0, canvas.width, canvas.height);
+  requestAnimationFrame(animate);
+  if (ariel.x < shell.x + shell.width &&
+    ariel.x + ariel.width > shell.x &&
+    ariel.y < shell.y + shell.height &&
+    ariel.height + ariel.y > shell.y) {
+    shell.isVisible = false;
+    // alert(`${points} points`);
+    console.log("collide with shell");
+    points += 10;
+    // gameOver = true;
+    // ariel.style.display="none";
+    // c.shell.remove();
+    // shell.splice(i);
+  }
+  if (ariel.x < anchor.x + anchor.width &&
+    ariel.x + ariel.width > anchor.x &&
+    ariel.y < anchor.y + anchor.height &&
+    ariel.height + ariel.y > anchor.y) {
+    // alert(`game over: ${points} points`);
+    gameOver = true;
+    // console.log("hook!");
+    // alert("argh! merbabe got hooked!")
+  }
+  if (ariel.x < clam.x + clam.width &&
+    ariel.x + ariel.width > clam.x &&
+    ariel.y < clam.y + clam.height &&
+    ariel.height + ariel.y > clam.y) {
+    clam.isVisible = false;
+    // alert(`${points} points`);
+    points += 20;
+    console.log("clam");
+  }
+  if (ariel.x < snail.x + snail.width &&
+    ariel.x + ariel.width > snail.x &&
+    ariel.y < snail.y + snail.height &&
+    ariel.height + ariel.y > snail.y) {
+    snail.isVisible = false;
+    // alert(`${points} points`);
+    points += 5;
+    console.log("snail");
+    // console.log('snail overlapped');
+    // snail.visible = false;
+    // snail = null;
+    // console.log("snail!");
+  }
+  c.drawImage(backgroundImage, 0, 0, canvas.width, canvas.height);
+
+  if (shell.isVisible)
+    c.drawImage(shellImage, shell.x, shell.y, 800, 800);
 
   if (anchor.isVisible)
-c.drawImage(anchorImage, anchor.x, anchor.y, 200, 200);
+    c.drawImage(anchorImage, anchor.x, anchor.y, 400, 400);
 
-if (clam.isVisible)
-c.drawImage(clamImage, clam.x, clam.y, 100, 100);
+  if (clam.isVisible)
+    c.drawImage(clamImage, clam.x, clam.y, 500, 500);
 
-if (snail.isVisible)
-  c.drawImage(snailImage, snail.x, snail.y, 700, 700);
-  if (shell.isVisible)
+  if (snail.isVisible)
+    c.drawImage(snailImage, snail.x, snail.y, 700, 700);
 
-  if (ariel.isVisible)
-c.drawImage(arielImage, ariel.x, ariel.y, 300, 300);
-// c.drawImage(dangerImage, danger.x, danger.y, 100, 100);
+    if (ariel.isVisible)
+      c.drawImage(arielImage, ariel.x, ariel.y, 400, 400);
+  // c.drawImage(dangerImage, danger.x, danger.y, 100, 100);
 
 }
 
 /// trying switch case for moving ARIEL
 function moveAriel(e) {
-    switch(e.which) {
-        case 37:
-        if(ariel.x - ariel.dx > 0){
+  switch (e.which) {
+    case 37:
+      if (ariel.x - ariel.dx > 0) {
         ariel.x -= ariel.dx;
       }
-        // console.log(ariel);
-            break;
-            // left key pressed
-        case 39:
-        if(ariel.x + ariel.dx < canvas.width) {
+      // console.log(ariel);
+      break;
+      // left key pressed
+    case 39:
+      if (ariel.x + ariel.dx < canvas.width) {
         ariel.x += ariel.dx;
       }
-            break;
-            // right key pressed
-        case 38:
-        if(ariel.y - ariel.dy > 0){
+      break;
+      // right key pressed
+    case 38:
+      if (ariel.y - ariel.dy > 0) {
         ariel.y -= ariel.dy;
       }
-            break;
-            // move up
-        case 40:
-        if(ariel.y + ariel.dy < canvas.height){
+      break;
+      // move up
+    case 40:
+      if (ariel.y + ariel.dy < canvas.height) {
         ariel.y += ariel.dy;
       }
-            break;
-            // down key pressed
-    }
+      break;
+      // down key pressed
+  }
 }
 
-if (!gameOver)
+if (!gameOver);
   animate();
 
 
@@ -253,15 +264,6 @@ document.onkeydown = moveAriel;
 //   var x = Math.random() * window.innerWidth;
 //   var y = Math.random() * window.innerHeight;
 // }
-
-
-
-
-
-
-
-
-
 
 
 
