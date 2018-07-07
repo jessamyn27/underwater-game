e19f3c3b20a69fdd94d994deb63503c4f9f198fd
Jessamyn McTwigan <jessamyn.mctwigan@gmail.com>
2018-07-07 18:48:59 -0500

    timer interaction with game

--- app.js
+++ app.js
@@ -16,7 +16,11 @@ window.addEventListener('resize', function() {
 // Listens for keydown
 window.addEventListener("keydown", moveAriel, true);
 var gameOver = false;
-var points = 0;
+// var points = 0;
+$('#Ariel').on('click', (e) => {
+console.log(ariel);
+startTimer() = true;
+});
 
 function startTimer(duration, display) {
     var timer = duration, minutes, seconds;
@@ -27,19 +31,31 @@ function startTimer(duration, display) {
         minutes = minutes < 10 ? "0" + minutes : minutes;
         seconds = seconds < 10 ? "0" + seconds : seconds;
 
-        display.textContent = minutes + ":" + seconds;
+        display.textContent = "Hurry Merbabe! You only have " + ":" + seconds + " Seconds!";
 
         if (--timer < 0) {
             timer = duration;
         }
     }, 1000);
+        if (timer = 0) {
+          display.textContent = "time is up!";
+        }
 }
 
 window.onload = function () {
     var halfMinute = 30 * 1,
         display = document.querySelector('#time');
     startTimer(halfMinute, display);
-};
+}
+
+// function stopTimer(duration, display) {
+//     var = timer(function(){ alert("Hello"); }, 3000);
+// }
+
+function stopTimer() {
+    clearTimer(duration, seconds);
+    startTimer() = false;
+}
 
 //CLASS
 class Floater {
@@ -165,7 +181,7 @@ const animate = () => {
     shell.isVisible = false;
     // alert(`${points} points`);
     // console.log("collide with shell");
-    points += 10;
+    // points += 10;
     // gameOver = true;
     // ariel.style.display="none";
     // c.shell.remove();
@@ -196,7 +212,7 @@ const animate = () => {
     ariel.height + ariel.y > clam.y) {
     clam.isVisible = false;
     // alert(`${points} points`);
-    points += 20;
+    // points += 20;
     // console.log("clam");
   }
   if (ariel.x < snail.x + snail.width &&
@@ -205,7 +221,7 @@ const animate = () => {
     ariel.height + ariel.y > snail.y) {
     snail.isVisible = false;
     // alert(`${points} points`);
-    points += 5;
+    // points += 5;
     // console.log("snail");
     // console.log('snail overlapped');
     // snail.visible = false;
@@ -268,191 +284,4 @@ if (!gameOver);
   // alert("hooked!")
   animate();
 
-
-
-// axis-aligned bounding box to make objects collide:
-
-// var rect1 = {x: 5, y: 5, width: 50, height: 50}
-// var rect2 = {x: 20, y: 10, width: 10, height: 10}
-
-
-//
-// filling in the values =>
-//
-// if (5 < 30 &&
-//     55 > 20 &&
-//     5 < 20 &&
-//     55 > 10) {
-//     collision detected!
-// }
-
 document.onkeydown = moveAriel;
-// document.onkeydown = moveAnchor;
-// document.onkeydown = moveShell;
-
-// move hook randomly around the room
-
-
-
-
-
-// need to make x amt of pink shells float around at random in the canvas
-
-// const pinkShell =
-// for (var i = 0; i < 400; i++) {
-//   var x = Math.random() * window.innerWidth;
-//   var y = Math.random() * window.innerHeight;
-// }
-
-
-
-
-
-
-
-
-
-// c.fillStyle = "rgba(255, 0, 0, 0.5)";
-// c.fillRect(100, 100, 100, 100);
-// c.fillStyle = "rgba(0, 0, 255, 0.5)";
-// c.fillRect(400, 400, 100, 100);
-// c.fillStyle = "rgba(0, 255, 0, 0.5)";
-// c.fillRect(300, 300, 100, 100);
-//
-// console.log(canvas);
-
-// line
-// c.beginPath();
-// c.moveTo(50, 300);
-// c.lineTo(300, 100);
-// c.lineTo(300, 100);
-// c.lineTo(400, 300);
-// c.strokeStyle = "lavender";
-// c.stroke();
-
-/////////////////////////////////// circles ////////
-
-// arc or circle x, y, radius, startAngle endAngle draw counter clockwise
-// c.beginPath();
-// c.arc(300, 300, 30, 0, Math.PI * 2, false);
-// c.strokeStyle = 'blue';
-// c.stroke();
-// //
-// for (var i = 0; i < 100; i++) {
-//   var x = Math.random() * window.innerWidth;
-//   var y = Math.random() * window.innerHeight;
-//   c.beginPath();
-//   c.arc(x, y, 30, 0, Math.PI * 2, false);
-// c.strokeStyle = 'blue';
-// c.stroke();
-// }
-// var mouse = {
-//   x: undefined,
-//   y: undefined
-// }
-// var maxRadius = 40;
-// // var minRadius = 2;
-//
-// var colorArray = [
-//   'pink',
-//   'lavender',
-//   'beige',
-//   'blue',
-//   'green',
-// ]
-//
-// window.addEventListener('mousemove',
-// function (event) {
-// mouse.x = event.x;
-// mouse.y = event.y;
-// })
-//
-// window.addEventListener('resize', function()
-// {
-//   canvas.width = window.innerWidth;
-//   canvas.height = window.innerHeight;
-//
-//   // itit();
-//
-// });
-//
-// function Circle(x, y, dx, dy, radius) {
-//   this.x = x;
-//   this.y = y;
-//   this.dx = dx;
-//   this.dy = dy;
-//   this.radius = radius;
-//   this.minRadius = radius;
-//   this.color = colorArray[Math.floor(Math.random() * colorArray.length)];
-//
-//   this.draw = function() {
-//     c.beginPath();
-//     c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false
-//       );
-//     c.strokeStyle = this.color;
-//     c.stroke();
-//     // c.fill():
-//
-//   }
-//
-//   this.update = function() {
-//     if (this.x + this.radius > innerWidth ||
-//       this.x - this.radius < 0) {
-//       this.dx = -this.dx;
-//     }
-//     if (this.y + this.radius > innerHeight ||
-//       this.y - this.radius < 0) {
-//       this.dy = -this.dy;
-//     }
-//     this.x += this.dx;
-//     this.y += this.dy;
-//
-// // this is interactivity with the mouse
-//   if (mouse.x - this.x < 50 && mouse.x - this.x > -50
-//     && mouse.y - this.y < 50 && mouse.y - this.y > -50)
-//      {
-//        if (this.radius < maxRadius) {
-//          this.radius += 1;
-//
-//        }
-//   } else if (this.radius > this.minRadius) {
-//       this.radius -= 1;
-//
-//   }
-//
-//     this.draw();
-//   }
-// }
-//
-//
-//
-// var circleArray = [];
-//
-// // function init() {
-//   for (var i = 0; i < 8000; i++) {
-//   var radius = Math.random() * 3 + 1;
-//   var x = Math.random() * (innerWidth - radius * 2) + radius;
-//   var y = Math.random() * (innerHeight - radius * 2) + radius;
-//   var dx = (Math.random() -0.5) * 2;
-//   var dy = (Math.random() -0.5) * 2;
-//   circleArray.push(new Circle(x, y, dx, dy, radius));
-//   }
-// // }
-//
-// function animate() {
-//   requestAnimationFrame(animate);
-//   c.clearRect(0, 0, innerWidth, innerHeight
-//     );
-//     for (var i = 0; i < circleArray.length; i++) {
-//       circleArray[i].update();
-//     }
-// }
-//   c.beginPath();
-//   c.arc(x, y, radius, 0, Math.PI * 2, false
-//     );
-//   c.strokeStyle = 'blue';
-//   c.stroke();
-//
-// itit();
-//
-// animate();
